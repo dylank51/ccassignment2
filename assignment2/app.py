3a4d2389d072a18ea32b586d2339cb976a48e470
koybz <c15523157@manager.c.c15523157.internal>
2017-12-04 00:07:19 +0000

    1

---
+++ assignment2/app.py
@@ -0,0 +1,31 @@
+ each['ports'] = c[-2]
+        all.append(each)
+    return all
+
+#
+# Parses the output of a Docker logs command to a python Dictionary
+# (Key Value Pair object)
+def docker_logs_to_object(id, output):
+    logs = {}
+    logs['id'] = id
+    all = []
+    for line in output.splitlines():
+        all.append(line)
+    logs['logs'] = all
+    return logs
+
+#
+# Parses the output of a Docker image command to a python List
+# 
+def docker_images_to_array(output):
+    all = []
+    for c in [line.split() for line in output.splitlines()[1:]]:
+        each = {}
+        each['id'] = c[2]
+        each['tag'] = c[1]
+        each['name'] = c[0]
+        all.append(each)
+    return all
+
+if __name__ == "__main__":
+app.run(host="0.0.0.0",port=5000, debug=True)
